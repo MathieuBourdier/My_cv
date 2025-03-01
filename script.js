164d98735190ff6b1ad0560c8c596470f51f9613
Mathieu Bourdier <math.9408@gmail.com>
2025-03-01 22:35:07 +0100

    adding a chatbox

--- script.js
+++ script.js
@@ -82,3 +82,44 @@ darkModeBtn.addEventListener("click", () => {
         localStorage.setItem("darkMode", "disabled");
     }
 });
+
+/* script.js  for a chatbot */
+function sendQuestion(question) {
+    const chatBox = document.getElementById("chat-box");
+    const userMessage = `<div class='chat-message user'>${question}</div>`;
+    chatBox.innerHTML += userMessage;
+    
+    setTimeout(() => {
+        let response = "";
+        switch(question) {
+            case "Que recherchez vous comme poste ?":
+                response = "Je suis à la recherche d’un stage de développeur d'une durée de six mois.";
+                break;
+            case "Quand etes vous disponnible ?":
+                response = "Je suis actuellement en formation chez Donkeyschool et disponible dès le 16 septembre 2025.";
+                break;
+            case "Comment vous contacter ?":
+                response = "Vous pouvez me joindre par mail à math.9408@gmail.com.";
+                break;
+            /* QUESTION A RAJOUTER case "Comment vous contacter ?":
+                response = "Vous pouvez me joindre par mail à math.9408@gmail.com.";
+                break;; */
+            default:
+                response = "Je n’ai pas compris votre question.";
+        }
+        chatBox.innerHTML += `<div class='chat-message bot'>${response}</div>`;
+        chatBox.scrollTop = chatBox.scrollHeight;
+    }, 1000);
+}
+
+function toggleChat() {
+    const chatContainer = document.getElementById("chat-container");
+    const toggleIcon = document.getElementById("toggle-icon");
+    chatContainer.classList.toggle("minimized");
+
+    if (chatContainer.classList.contains("minimized")) {
+        toggleIcon.textContent = "▲";
+    } else {
+        toggleIcon.textContent = "▼";
+    }
+}
